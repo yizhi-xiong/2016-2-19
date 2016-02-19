3d386543e27a7c43e8360ce16b891fc4222d9dac
yizhi-xiong <thomas.yizhi.xiong@gmail.com>
2016-02-19 11:19:24 -0800

    json.js

---
+++ json.js
@@ -0,0 +1,15 @@
+title="何曜日";
+d3.select("title").html(title);
+d3.select("body").append("h1").html(title).style("color","blue");
+
+何曜日 = [];
+何曜日.push("日曜日");
+何曜日.push("月曜日");
+何曜日.push("火曜日");
+何曜日.push("水曜日");
+何曜日.push("木曜日");
+何曜日.push("金曜日");
+何曜日.push("土曜日");
+console.log(何曜日);
+JSON.stringify(何曜日);
+alert(何曜日);
